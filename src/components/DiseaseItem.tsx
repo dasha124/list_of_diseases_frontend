c13301904e9728ae0e4180d284200834887d7c72
dasha124 <dashakonekb@gmail.com>
2023-11-13 08:14:52 +0000

    cards_1

--- src/components/DiseaseItem.tsx
+++ src/components/DiseaseItem.tsx
@@ -1,7 +1,8 @@
-import { FC, useState, useEffect} from 'react'
+// import { FC, useState, useEffect} from 'react'
+import {FC} from 'react'
 import { Button, Card } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Buffer } from 'buffer';
+// import { Buffer } from 'buffer';
 import "/home/student/reactProject/list_of_diseases/src/components/ds.css"
 
 
@@ -17,68 +18,68 @@ interface Disease {
 // const DiseaseItem: FC<Disease> = ({ image64, disease_name, disease_id}) => {
 const DiseaseItem: FC<Disease> = ({ disease_name, disease_id}) => {
     
-    const [image, setImage64] = useState('');
+    // const [image, setImage64] = useState('');
 
+
+    // // useEffect(() => {
+    // //     // 1/ const image = image64
+    // //     // setImage64(image)
+
+
+    // //     // 2/ 
+    // //     const buffer: Buffer = Buffer.from(String(image64));
+    // //     const base64String: string = buffer.toString('base64');
+        
+    // //     setImage64(base64String);
+    // // }, []);
+    // // console.log("image =", typeof(image))
+
+
+    // const fetchData = async () => {
+        
+
+    //     const response = await fetch('/api/diseases/');
+    //     const data = await response.json();
+    // //     // console.log("data", data)
+    // //     // for(let i = 0; i < data.length; i++) {
+    // //     //     data[i].image64 = String(data[i].image64)
+            
+    // //     //     // const bytes = new Uint8Array(atob(data[i].image).split('').map(c => c.charCodeAt(0)));
+    // //     //     // console.log(bytes)
+
+
+    // //     //     // const base64String: string = /* ваша строка в формате base64 */;
+    // //     //     const imageB: Buffer = Buffer.from(data[i].image64, 'base64');
+    // //     //     data[i].image64 = imageB
+    // //     //     console.log("B =", data[i].image64)
+    // //     //     // setImage64(data.image64);
+    // //     //     // data[i].image = decoder.decode(data[i].image);
+    // //     //     // data[i].image = data[i].image.substring(1, data[i].image.length - 1)
+    // //     //   }
+        
+    //     // data.image = String(data.image)
+    //     // console.log(data.image)
+    //     // const imageB: Buffer = Buffer.from(data.image, 'base64');
+    //     // data.image = imageB
+    //     // console.log("B =", data.image)
+
+
+    //     // const decodedImage: string = atob(data.image64);
+    //     // console.log(decodedImage);
+        
+    //     const buffer: Buffer = Buffer.from(String(data.image64));
+    //     console.log("0 =", buffer)
+    //     const base64String: string = buffer.toString('base64');
+    //     console.log("1 =", base64String)
+
+    //     setImage64(data.image); // Обновление значения image64 с помощью setImage64
+
+    // }
 
     // useEffect(() => {
-    //     // 1/ const image = image64
-    //     // setImage64(image)
 
-
-    //     // 2/ 
-    //     const buffer: Buffer = Buffer.from(String(image64));
-    //     const base64String: string = buffer.toString('base64');
-        
-    //     setImage64(base64String);
+    //     fetchData();
     // }, []);
-    // console.log("image =", typeof(image))
-
-
-    const fetchData = async () => {
-        
-
-        const response = await fetch('/api/diseases/');
-        const data = await response.json();
-    //     // console.log("data", data)
-    //     // for(let i = 0; i < data.length; i++) {
-    //     //     data[i].image64 = String(data[i].image64)
-            
-    //     //     // const bytes = new Uint8Array(atob(data[i].image).split('').map(c => c.charCodeAt(0)));
-    //     //     // console.log(bytes)
-
-
-    //     //     // const base64String: string = /* ваша строка в формате base64 */;
-    //     //     const imageB: Buffer = Buffer.from(data[i].image64, 'base64');
-    //     //     data[i].image64 = imageB
-    //     //     console.log("B =", data[i].image64)
-    //     //     // setImage64(data.image64);
-    //     //     // data[i].image = decoder.decode(data[i].image);
-    //     //     // data[i].image = data[i].image.substring(1, data[i].image.length - 1)
-    //     //   }
-        
-        // data.image = String(data.image)
-        // console.log(data.image)
-        // const imageB: Buffer = Buffer.from(data.image, 'base64');
-        // data.image = imageB
-        // console.log("B =", data.image)
-
-
-        // const decodedImage: string = atob(data.image64);
-        // console.log(decodedImage);
-        
-        const buffer: Buffer = Buffer.from(String(data.image64));
-        console.log("0 =", buffer)
-        const base64String: string = buffer.toString('base64');
-        console.log("1 =", base64String)
-
-        setImage64(data.image); // Обновление значения image64 с помощью setImage64
-
-    }
-
-    useEffect(() => {
-
-        fetchData();
-    }, []);
 
 
 
