ae277dc35e67d446031028670bb2c6201b03ca0d
dasha124 <dashakonekb@gmail.com>
2024-01-16 17:29:29 +0000

    add hp_

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DiseaseCard from './DiseaseCard.tsx'
